79cc23affe5faa4f4d86dd29a0e54fa54f5489d0
Adam Mawlawi <adam@mradam.us>
2022-04-09 13:41:47 -0500

    create button components using styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,27 @@
+import { Button } from "components/common"
+import { createGlobalStyle } from "styled-components"
+
+const GlobalStyle = createGlobalStyle`
+  body{
+    background: white;
+    min-height: 100vh;
+    margin: 0;
+    color: black;
+    font-family: 'Kaushan Script';
+  }
+
+`
+
 function App() {
-  return <h1>App</h1>
+  return (
+    <>
+      <GlobalStyle />
+      <h1>App</h1>
+      <Button>Primary</Button>
+      <Button secondary>secondary</Button>
+      <Button large>Large</Button>
+    </>
+  )
 }
 
 export default App
