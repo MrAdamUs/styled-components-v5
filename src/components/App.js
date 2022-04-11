dea8738ef2fcb3866cd55e315ed0d634ac758b2d
Adam Mawlawi <adam@mradam.us>
2022-04-11 08:42:36 -0500

    finshing up themes

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,32 @@
+import React, { useState } from "react"
 import { createGlobalStyle, ThemeProvider } from "styled-components"
 import { BrowserRouter, Switch, Route } from "react-router-dom"
 import Login from "components/pages/Login"
 import Home from "components/pages/Home"
+import LightTheme from "themes/light"
+import DarkTheme from "themes/dark"
 
 const GlobalStyle = createGlobalStyle`
   body{
-    background: white;
+    background: ${(p) => p.theme.bodyBackgroundColor};
     min-height: 100vh;
     margin: 0;
-    color: black;
+    color: ${(p) => p.theme.bodyFontColor};
     font-family: 'Kaushan Script';
   }
 
 `
-
-const theme = {
-  primaryColor: "#f8049c",
-  secondaryColor: "#fdd54f",
-}
-
 function App() {
+  const [theme, setTheme] = useState(LightTheme)
   return (
-    <ThemeProvider theme={theme}>
+    <ThemeProvider
+      theme={{
+        ...theme,
+        setTheme: () => {
+          setTheme((s) => (s.id === "light" ? DarkTheme : LightTheme))
+        },
+      }}
+    >
       <GlobalStyle />
       <BrowserRouter>
         <Switch>
